7ca95d5d4184db8a1eec95a3c0e0abf6804e62b2
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-17 15:04:03 -0300

    Creating worker application

---
+++ worker-api/src/test/java/com/github/omtcnvs/worker/api/WorkerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.omtcnvs.worker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
