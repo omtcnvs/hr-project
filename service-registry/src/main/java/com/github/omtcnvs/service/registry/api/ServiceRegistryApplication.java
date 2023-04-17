7dc19d4ef1932cc4d5c3afd6bcd1d5f4e2b236f5
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-17 14:32:36 -0300

    Configurating service-regsistry application

--- service-registry/src/main/java/com/github/omtcnvs/service/registry/api/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/github/omtcnvs/service/registry/api/ServiceRegistryApplication.java
@@ -2,7 +2,9 @@ package com.github.omtcnvs.service.registry.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 
