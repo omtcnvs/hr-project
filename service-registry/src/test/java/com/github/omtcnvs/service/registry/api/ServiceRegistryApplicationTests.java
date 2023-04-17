4b14814e690a858f42b9d1d345ce1f3eae634319
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-17 14:30:08 -0300

    Creating service-registry application

---
+++ service-registry/src/test/java/com/github/omtcnvs/service/registry/api/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.omtcnvs.service.registry.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
