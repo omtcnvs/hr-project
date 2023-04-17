5851805399225e90e8fb87bee544e6caf74278c6
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-17 14:40:39 -0300

    Creating server-config application

---
+++ server-config/src/main/java/com/github/omtcnvs/server/config/api/ServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.github.omtcnvs.server.config.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerConfigApplication.class, args);
+	}
+
+}
---
+++ server-config/src/test/java/com/github/omtcnvs/server/config/api/ServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.omtcnvs.server.config.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
