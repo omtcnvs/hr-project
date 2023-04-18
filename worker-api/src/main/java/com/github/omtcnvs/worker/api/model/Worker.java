374f3965338ec9ed12bb220edae2edb60321d81f
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-17 22:30:53 -0300

    Criando model worker-api

---
+++ worker-api/src/main/java/com/github/omtcnvs/worker/api/model/Worker.java
@@ -0,0 +1,25 @@
+package com.github.omtcnvs.worker.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_worker")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Worker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double dailyIncome;
+
+}
